5832d426039e5b7c53cd532e39d65188168475f1
peter-gao <peter811031@gmail.com>
2013-12-12 00:17:33 +0800

    3rd-1

--- src/Painter/ToolBar.java
+++ src/Painter/ToolBar.java
@@ -6,41 +6,39 @@ import java.awt.*;
 import java.awt.event.*; //滑鼠事件監聽器
 
 
-public class ToolBar extends JPanel {
+public class ToolBar extends JPanel{
     
+    Color c ;
     MainWindows parent = null;
     JButton stopDrawBtn;
     JButton drawLineBtn;
     JButton exitBtn;
     JButton drawPen;
-    
-    
+    final JButton setColor;
+   
     ToolBar(MainWindows p)
-    {
+    {   
         parent = p;
         this.setBackground(Color.red);
         //設定工具欄背景顏色 : 紅
-        
-        this.setLayout(new FlowLayout(FlowLayout.LEFT));
-        //在ToolBar介面上新增  ""FlowLayout()""<--介面管理員
-        
+       
         exitBtn = new JButton("EXIT");
         drawLineBtn = new JButton("Draw Lines");
         stopDrawBtn = new JButton("Stop Draw");
         drawPen = new JButton("pen");
-        
         //新增按鈕
-        stopDrawBtn.setEnabled(false);
         
-      /*  this.setLayout(new BorderLayout());
-        //在ToolBar介面上新增  ""BorderLayout()""<--介面管理員
-      */
+        setColor = new JButton("  ");
+        
+        stopDrawBtn.setEnabled(false);
         
         this.add(exitBtn,BorderLayout.WEST);
         //在ToolBar介面上加入 按鈕-->""exitBtn""  並且讓按鈕設定在西邊
         this.add(drawLineBtn);
         this.add(stopDrawBtn);
         this.add(drawPen);
+        this.add(setColor);
+       
         
         drawLineBtn.addMouseListener(
             new MouseAdapter()
@@ -96,5 +94,19 @@ public class ToolBar extends JPanel {
             }
          );
         //在按鈕-->""exitBtn""下 加入滑鼠監聽 設定只要按了 "exitBtn" 就會跳出 
+ 
+          setColor.addMouseListener(
+            new MouseAdapter()
+            {
+                public void mouseClicked(MouseEvent e)
+                {
+                      c = JColorChooser.showDialog(null,"Choose A Color", setColor.getBackground());
+                     if (c != null) 
+                     {setColor.setBackground(c);}
+                }
+            }
+         ); 
+        
     }
+         
 }
